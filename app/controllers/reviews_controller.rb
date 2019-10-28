7b0362685d55a2764dfd580faa10d3564bd0cfb2
slvk14 <slavikslavik1414@gmail.com>
2019-10-28 12:00:16 +0200

    reviews display edited

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,18 +5,18 @@ class ReviewsController < ApplicationController
   after_action :verify_authorized
 
   def index
-    @reviews = Movie.find(params[:movie_id]).reviews
+    @reviews = movie.reviews
     authorize @reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
+    @movie = movie
     @review = @movie.reviews.new
     authorize @review
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
+    @movie = movie
     @review = @movie.reviews.build(review_params)
     authorize @review
     if @review.save
@@ -29,16 +29,18 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-    @review = resource
+    @movie = movie
+    @review = @movie.reviews.find(params[:id])
     authorize @review
   end
 
   def update
-    @review = resource
+    @movie = movie
+    @review = @movie.reviews.find(params[:id])
     authorize @review
     if @review.update(review_params)
       flash.now[:notice] = 'Review updated!'
-      redirect_to index_path
+      redirect_to movie_path(movie.id)
     else
       flash.now[:error] = 'Failed to update review!'
       render :edit
@@ -46,19 +48,24 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @review = resource
+    @movie = movie
+    @review = @movie.reviews.find(params[:id])
     authorize @review
     if @review.delete
       flash.now[:notice] = 'Review deleted!'
     else
       flash.now[:error] = 'Failed to delete this review!'
     end
-    redirect_to index_path
+    redirect_to movie_path(movie.id)
   end
 
   private
 
   def review_params
     params.require(:review).permit(:rate, :description, :content).merge!(user_id: current_user.id)
+  end
+
+  def movie
+    Movie.find(params[:movie_id])
   end
 end
